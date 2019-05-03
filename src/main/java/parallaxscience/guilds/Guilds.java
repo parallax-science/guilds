63307da076f257490d4bc10902c41198bfca1692
Tristan Jay <capdincrando@gmail.com>
2019-05-03 13:58:59 -0500

    Added Minecraft Forge

---
+++ src/main/java/parallaxscience/guilds/Guilds.java
@@ -0,0 +1,13 @@
+package parallaxscience.guilds;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+public class Guilds
+{
+    public static final String MODID = "guilds";
+    public static final String NAME = "The Guilds Mod";
+    public static final String VERSION = "ALPHA 1.0";
+}
