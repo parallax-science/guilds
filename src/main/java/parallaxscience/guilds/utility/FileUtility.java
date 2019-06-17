02bba569a69bb68e2a132c840c41c789a76cb8e9
Tristan Jay <capdincrando@gmail.com>
2019-06-16 21:04:49 -0500

    Updated JavaDoc

--- src/main/java/parallaxscience/guilds/utility/FileUtility.java
+++ src/main/java/parallaxscience/guilds/utility/FileUtility.java
@@ -4,8 +4,15 @@ import java.io.File;
 
 public class FileUtility
 {
+	/**
+	 * Guild mod save directory
+	 */
 	public static final String guildDirectory = "world/guilds";
 
+	/**
+	 * Checks to see if guild directory has been created
+	 * If not, creates the directory
+	 */
 	@SuppressWarnings("ResultOfMethodCallIgnored")
 	public static void directoryCheck()
 	{
